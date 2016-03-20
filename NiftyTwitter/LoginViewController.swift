e9306f510c4c998004247dc9f2cb35e23343aa8f
kosuge_satoshi <k.satoshi.swift@gmail.com>
2016-03-20 22:37:14 +0900

    add UIButton and UITextField to interface Builder of LoginViewController

---
+++ NiftyTwitter/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  NiftyTwitter
+//
+//  Created by 小菅仁士 on 2016/03/20.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    @IBAction func tapLoginButton(sender: UIButton) {
+        
+    }
+    
+    @IBAction func tapSignUpButton(sender: UIButton) {
+        
+    }
+}
