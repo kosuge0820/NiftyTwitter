138cdc19fd23f3d265d75b7a6cc02af413db22b0
小菅仁士 <satoshi02015@gmail.com>
2016-03-20 14:42:30 +0900

    Initial Commit

---
+++ NiftyTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NiftyTwitter
+//
+//  Created by 小菅仁士 on 2016/03/20.
+//  Copyright © 2016年 kosuge satoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
